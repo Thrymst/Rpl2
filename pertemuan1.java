8024b81b66408926bcf91fe69437bea0733927d1
Jun <115964158+Thrymst@users.noreply.github.com>
2024-11-15 16:34:38 +0700

    pertemuan 1=2

---
+++ pertemuan1.java
@@ -0,0 +1,13 @@
+public class pertemuan1 {
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        String nama = "Junaedi Samandias";
+        int umur = 21;
+        String kampus = "Gunadarma";
+
+
+        System.out.println("Perkenalkan nama saya " + nama );
+        System.out.println("Umur saya " + umur );
+        System.out.println("Saya berkuliah di " + kampus );
+    }
+}
