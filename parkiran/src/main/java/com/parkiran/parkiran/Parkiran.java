b1540d304db1eb4f17f5b8e3db5b67bc5cea58db
Jun <115964158+Thrymst@users.noreply.github.com>
2024-11-15 14:02:14 +0700

    Add files via upload
    folder

---
+++ parkiran/src/main/java/com/parkiran/parkiran/Parkiran.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.parkiran.parkiran;
+
+/**
+ *
+ * @author Jun
+ */
+public class Parkiran {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ parkiran/src/main/java/com/parkiran/repository/ParkiranRepository.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.parkiran.repository;
+import com.parkiran.model.ModelParkiran;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframwork.stereotype.Repository;
+/**
+ *
+ * @author Jun
+ */
+@Repository
+public interface ParkiranRepository extends JpaRepository<ModelParkiran, Long>{
+    public Optional<ModelParkiran> findByPlatNomor(String platNomor);
+}
